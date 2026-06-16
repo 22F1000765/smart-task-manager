4816b5165998eebf685cdb9f34f8cf8bc7652bdd
Pranitha <pranithamarimuthu251@gmail.com>
2026-06-16 17:54:38 +0530

    Implemented in-memory task CRUD

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.routers.task import router as task_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Smart Task Manager API",
+    version="1.0.0"
+)
+
+app.include_router(task_router)
+
 
 @app.get("/")
 def home():
---
+++ app/routers/task.py
@@ -0,0 +1,86 @@
+from fastapi import APIRouter, HTTPException
+from app.schemas.task import TaskCreate, TaskUpdate
+
+router = APIRouter(tags=["Tasks"])
+
+tasks = []
+task_id_counter = 1
+
+
+@router.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@router.get("/tasks/{task_id}")
+def get_task(task_id: int):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@router.post("/tasks")
+def create_task(task: TaskCreate):
+
+    global task_id_counter
+
+    new_task = {
+        "id": task_id_counter,
+        "title": task.title,
+        "description": task.description
+    }
+
+    tasks.append(new_task)
+
+    task_id_counter += 1
+
+    return {
+        "message": "Task created successfully",
+        "task": new_task
+    }
+
+
+@router.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+
+    for task in tasks:
+
+        if task["id"] == task_id:
+
+            task["title"] = updated_task.title
+            task["description"] = updated_task.description
+
+            return {
+                "message": "Task updated successfully",
+                "task": task
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@router.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+
+    for task in tasks:
+
+        if task["id"] == task_id:
+
+            tasks.remove(task)
+
+            return {
+                "message": "Task deleted successfully"
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
