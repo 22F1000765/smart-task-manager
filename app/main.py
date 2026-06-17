1a42e43adc4ed77dccc9ad6b87780f22728562db
Pranitha <pranithamarimuthu251@gmail.com>
2026-06-17 12:28:45 +0530

    Create Task Model and PostgreSQL table

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
+from app.database import Base,engine
+from app.models.task import Task
+
 from app.routers.task import router as task_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Smart Task Manager API",
---
+++ app/models/task.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    title: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    description: Mapped[str] = mapped_column(
+        String(500),
+        nullable=True
+    )
