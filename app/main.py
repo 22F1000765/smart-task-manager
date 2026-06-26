efbca18ac6ff9031714c19a6706b25174a12f318
Pranitha <pranithamarimuthu251@gmail.com>
2026-06-26 19:41:35 +0530

    Add CORS middleware for React frontend

--- app/main.py
+++ app/main.py
@@ -6,12 +6,28 @@ from app.models.task import Task
 from app.routers.task import router as task_router
 from app.routers.user import router as user_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Smart Task Manager API",
     version="1.0.0"
 )
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(task_router,
                    prefix="/tasks",
