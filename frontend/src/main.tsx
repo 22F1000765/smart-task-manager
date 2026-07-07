07aaa9a116c70d7a8d6e2094baf77efd0b2f6b09
Pranitha <pranithamarimuthu251@gmail.com>
2026-07-07 20:41:17 +0530

    feat: setup Tailwind CSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { AuthProvider } from "./context/AuthContext";
 
+import "./index.css"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
